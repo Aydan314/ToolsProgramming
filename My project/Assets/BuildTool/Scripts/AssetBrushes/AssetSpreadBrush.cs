16aee72095f1fddafc8ba58f911a7992a816c423
Aydan314 <113439420+Aydan314@users.noreply.github.com>
2025-03-18 13:49:20 +0000

    Asset spread brush continued

--- My project/Assets/BuildTool/Scripts/AssetBrushes/AssetSpreadBrush.cs
+++ My project/Assets/BuildTool/Scripts/AssetBrushes/AssetSpreadBrush.cs
@@ -1,7 +1,10 @@
 using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
 using UnityEditor.Experimental.GraphView;
 using UnityEditor.VersionControl;
 using UnityEngine;
+using static UnityEditor.Rendering.InspectorCurveEditor;
 
 public struct Bounds
 {
@@ -20,28 +23,116 @@ public class AssetSpreadBrush : AssetBaseBrush
 
         Bounds bounds = FindMinMaxBounds(Selection);
 
-        for (float x = bounds.min.x; x <= bounds.max.x; x+= assetPack.gridSize)
+        List<Bounds> boundsList = CreateSlices(Selection, assetPack, bounds);
+
+        foreach(Bounds b in boundsList)
         {
-            for (float y = bounds.min.z; y <= bounds.max.z; y+=assetPack.gridSize)
+            Debug.Log(b.min + " " + b.max);
+        }
+
+        for (float z = bounds.min.z; z < bounds.max.z; z += assetPack.gridSize)
+        {
+            List<Bounds> buildBetween = FindBoundsAtZ(z, boundsList);
+
+            buildBetween.OrderByDescending(b => b.min.x).ToList();
+
+            Debug.Log(buildBetween.Count);
+
+            for (int i = buildBetween.Count - 1; i > 0; i -= 2)
             {
-                if (Random.Range(0, 1000) / 1000.0f < assetPack.spreadDensity)
-                {
-                    PlaceAsset(asset.assetObject, root, new Vector3(x, Selection[0].nodePos.y, y), 0);
-                }
+                Vector3 min = buildBetween[i - 1].min;
+                Vector3 max = buildBetween[i].min;
+
+                min.z = z; max.z = z;
+
+                Debug.Log(min + " aaaaaaaaaaa " + max + " " + i);
+
+                BuildAssetsBetween(min, max, assetPack, root);
             }
         }
 
     }
 
-    public List<List<Bounds>> CreateSelectionLayers(List<BuildNode> selection)
+    public void BuildAssetsBetween(Vector3 start, Vector3 end, AssetBasePack assetPack, GameObject rootObject)
     {
-        List<List<Bounds>> selectionLayers = new List<List<Bounds>>();
+        Asset asset = assetPack.assets[0];
 
-        Bounds bounds = FindMinMaxBounds(selection);
+        float distance = (end - start).magnitude;
 
+        distance = assetPack.gridSize * Mathf.Round(distance / assetPack.gridSize);
 
+        Vector3 step = (end - start) / distance;
 
-        return selectionLayers;
+        for (float i = assetPack.gridSize; i < distance; i += assetPack.gridSize)
+        {
+            PlaceAsset(asset.assetObject, rootObject, start + (i * step), asset.defaultRotation);
+        }
+    }
+
+    public List<Bounds> FindBoundsAtZ(float z, List<Bounds> slices)
+    {
+        List<Bounds> found = new List<Bounds>();
+
+        foreach(Bounds bound in slices)
+        {
+            if (bound.min.z <= z && bound.max.z >= z)
+            {
+                found.Add(bound);
+            }
+        }
+
+        return found;
+    }
+
+    public List<Bounds> CreateSlices(List<BuildNode> selection, AssetBasePack assetPack, Bounds MinMax)
+    {
+        
+
+        List<Bounds> slices = new List<Bounds>();
+
+        for (float z = MinMax.min.z; z <= MinMax.max.z; z += assetPack.gridSize)
+        {
+            foreach (BuildNode node in selection)
+            {
+                if (node.nodePos.z == z)
+                {
+
+                    bool alreadyFound = false;
+
+                    foreach (Bounds bound in slices)
+                    {
+                        if (node.nodePos == bound.min || node.nodePos == bound.max) { alreadyFound = true; break; }
+                    }
+
+                    if (!alreadyFound)
+                    {
+                        Bounds slice = new Bounds();
+
+                        slice.min = node.nodePos;
+
+                        if (node.nextNode.nodePos.x == node.nodePos.x)
+                        {
+                            slice.max = node.nextNode.nodePos;
+                        }
+                        else
+                        {
+                            slice.max = node.prevNode.nodePos;
+                        }
+
+                        if (slice.max.z < slice.min.z)
+                        {
+                            Vector3 temp = slice.max;
+                            slice.max = slice.min;
+                            slice.min = temp;
+                        }
+
+                        slices.Add(slice);
+                    }
+                }
+            }
+        }
+
+        return slices;
     }
 
     public Bounds FindMinMaxBounds(List<BuildNode> selection)
